7a04bee2b098deb8590fdd3909e42372bf88a1d5
NickArthur <nickarthur.night@gmail.com>
2019-02-02 15:45:39 -0600

    Initial Commit

---
+++ Pauline2-African-StarterTests/Pauline2_African_StarterTests.swift
@@ -0,0 +1,34 @@
+//
+//  Pauline2_African_StarterTests.swift
+//  Pauline2-African-StarterTests
+//
+//  Created by Larry Nickerson on 2/2/19.
+//  Copyright © 2019 Lawrence Nickerson. All rights reserved.
+//
+
+import XCTest
+@testable import Pauline2_African_Starter
+
+class Pauline2_African_StarterTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
